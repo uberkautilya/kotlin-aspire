ee3137e8e4f906efa9664113bf0fe736b098f8ea
Mithun <mithuncm@gmail.com>
2023-02-09 17:27:26 +0530

    Named Parameter with User defined object

--- src/main/kotlin/userdefined/Polymorphism.kt
+++ src/main/kotlin/userdefined/Polymorphism.kt
@@ -2,10 +2,14 @@ package main.kotlin.userdefined
 
 fun main() {
     var animal: Animal = Animal ("Animal")
-    animal = Lion()
-    printAnimal(animal)
+    var lion = Lion()
+
+    printAnimal(animal2=lion, animal1 = animal)
+    //Polymorphism: Child object assigned to parent reference
+    animal = lion
 }
 
-fun printAnimal(animal: Animal) {
-    println("Animal: $animal")
+fun printAnimal(animal1: Animal, animal2: Animal) {
+    println("Animal 1: $animal1")
+    println("Animal 2: $animal2")
 }
