3ac93015cfd00d7aa3783bc6b6e63de286bc0367
Mithun <mithuncm@gmail.com>
2023-02-09 14:17:54 +0530

    Default and Named Parameters to allow optional parameters

---
+++ src/main/kotlin/DefaultAndNamedParameters.kt
@@ -0,0 +1,19 @@
+package main.kotlin
+
+fun main() {
+    printBorder('*', 10)
+    printBorder('(')
+    // When overriding the arguments in another order, use name of the parameters
+    printBorder(length = 20)
+}
+
+/**
+ * Default values are provided in the parameter declaration.
+ * If no values are provided at invocation, these values are assigned by default
+ */
+fun printBorder(char: Char = '=', length: Int = 10) {
+    for (i in 1..length) {
+        print("$char ")
+    }
+    println()
+}
---
+++ src/main/kotlin/OptionalParameters.kt
@@ -0,0 +1,9 @@
+package main.kotlin
+
+data class DataClassWithSecondaryConstructors(
+    val name: String, val surname: String, val age: Number
+) {
+    constructor() : this("", "Doe", Int.MIN_VALUE)
+    constructor(name: String) : this(name, "Deere", Int.MIN_VALUE)
+    constructor(name: String, surname: String) : this(name, surname, Int.MIN_VALUE)
+}
