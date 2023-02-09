9e04ba2f337def4f3c9bedf6c70d3449b54682ff
Mithun <mithun.mohandas@aspiresys.com>
2023-02-09 17:11:00 +0530

    Labelled return statement on a Lambda

--- src/main/kotlin/lambdas/ArrayEqualsWithLambdas.kt
+++ src/main/kotlin/lambdas/ArrayEqualsWithLambdas.kt
@@ -1,25 +1,23 @@
 package main.kotlin.lambdas
 
 fun main() {
-    var array1: Array<Int> = arrayOf(11, 2, 3, 4, 5)
-    var array2: Array<Int> = arrayOf(11, 2, 3, 4, 5)
+    val array1: Array<Int> = arrayOf(11, 2, 3, 4, 5, 6)
+    val array2: Array<Int> = arrayOf(11, 2, 3, 4, 4)
 
     val arrayEquals = arrayCheckLambda(array1, array2)
     println("Array Equals: $arrayEquals")
 }
 
-val arrayCheckLambda: (Array<Int>, Array<Int>) -> Boolean = { array1: Array<Int>, array2: Array<Int> ->
-    var bool: Boolean = true
-    bool = if (array1.size != array2.size) {
-        false
-    } else {
-        for (i in array1.indices) {
-            if (array1[i] != array2[i]) {
-                bool = false
-                break
-            }
+val arrayCheckLambda: (Array<Int>, Array<Int>) -> Boolean = arrayCheckLambda@{ array1: Array<Int>, array2: Array<Int> ->
+    var bool = true
+    if (array1.size != array2.size) {
+        return@arrayCheckLambda false
+    }
+    for (i in array1.indices) {
+        if (array1[i] != array2[i]) {
+            bool = false
+            break
         }
-        bool
     }
     bool
 }
