525a0706783585fe0ddc513add09dac077021b18
Mithun <mithuncm@gmail.com>
2023-02-13 12:50:17 +0530

    Functions on lists

---
+++ src/main/kotlin/day4/advancedfunctions/FunctionsOnLists.kt
@@ -0,0 +1,26 @@
+package main.kotlin.day4.advancedfunctions
+
+/**
+ * associate will associate the value with some other to return a map
+ * find(predicate) on lists can be used to find first occurrence - Nullable
+ */
+fun main() {
+    val list = listOf("Kotlin", "Java", "C++", "Javascript", null, null)
+    val map = list
+        .take(4)
+        .filterNotNull()
+        .filter { it.startsWith("J") }
+        .associate { it.length to it }
+//        .forEach { println("${it.key}: ${it.value}") }
+
+    val language = list.first()
+    println("language: ${language}")
+    val findResult = list.filterNotNull()
+        .find { it.startsWith("foo") }?:"Not found"
+    println("findResult: ${findResult}")
+
+    //orEmpty() method is available on String? to return ""
+    val stringOrEmpty = list.filterNotNull()
+        .findLast{it.startsWith("foo")}.orEmpty()
+    println("stringOrEmpty: ${stringOrEmpty}")
+}
