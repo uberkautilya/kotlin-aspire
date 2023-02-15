b765aca4c2700d9b98544c8bcef4d95bc798fecb
Mithun <mithun.mohandas@aspiresys.com>
2023-02-15 11:23:47 +0530

    Wednesday Exercise file init

---
+++ src/main/kotlin/wednesday/Wednesday.kt
@@ -0,0 +1,15 @@
+package main.kotlin.wednesday
+
+/**
+ * Points to be explored1.Can we able to pass user defined object in mutable list?
+ * 2.Can we have user defined objects in map?
+ * 3.Can we override toString method
+ * 4.Sorting in Map
+ * 5.Can we write user defined function in 'It'.
+ * 6.Enum class in kotlin 
+ * 7.Switch with enum 
+ * 8.Sealed class in kotlin
+ * 9.Can we able to covert dto to enity and vice versa in kotlin (Entity <-> dto)
+ * 10.Comaparator and Comparable in kotlin 
+ * Kotlin  -- Exception Handling
+ */
