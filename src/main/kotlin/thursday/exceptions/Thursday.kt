85be5b667e35c2dd4cafb9a2a3930cdc29206cb3
Mithun <mithuncm@gmail.com>
2023-02-16 10:30:16 +0530

    Thursday Commits: WIP

---
+++ src/main/kotlin/thursday/exceptions/Thursday.kt
@@ -0,0 +1,13 @@
+package main.kotlin.thursday.exceptions
+
+/**
+ * 1 . Can we override custom exception classes in Kotlin
+ * 2. ObjectMapper in Kotlin
+ * 3. Hashcode and equals in Map
+ * 4. What is copy method? Can we use it in dto to entity conversion
+ * 5. Try with resource - Kotlin
+ * 6. Synchronization in Kotlin
+ * 7. TreeSet - Kotlin
+ * 8. Spring AOP with Kotlin
+ * 9. Controller Layer, RequestBody, RequestParam - Kotlin
+ */
